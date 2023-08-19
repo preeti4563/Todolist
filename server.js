cd1b1bcf9fa5968a4cd41df431c947051aaacb75
preeti kumari <preeti.kumari@kraftshala.com>
2023-08-19 11:22:28 +0530

    todo list crud

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const nodemailer = require('nodemailer');
-const uuid = require('uuid');
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 // MySQL database connection configuration
 const db = mysql.createConnection({
@@ -16,106 +16,138 @@ const db = mysql.createConnection({
   database : 'test'
 });
 
-db.connect((err) => {
+db.connect(err => {
   if (err) {
-    console.error('Error connecting to MySQL database:', err);
+    console.error('Error connecting to MySQL:', err);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to MySQL');
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// JWT Secret Key
+const JWT_SECRET = 'your_secret_key';
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Middleware for JWT Authorization
+const authorize = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+
+// Create a new JWT token when a user logs in
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Authenticate user (replace with your authentication logic)
+  if (username === 'user123' && password === 'password123') {
+    const user = { id: 1, username: 'user123' };
+    const token = jwt.sign({ user }, JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
 });
 
+// Create a new Todo item
+app.post('/todos', authorize, (req, res) => {
+  const { title, description } = req.body;
 
-// Register a new user
-app.post('/register', (req, res) => {
-  const { firstName, lastName, email, phoneNumber } = req.body;
-
-  // Generate OTP
-  const otp = Math.floor(100000 + Math.random() * 900000);
-
-  // Save the user and OTP to the database
-  const userId = uuid.v4();
-  const userData = {
-    id: userId,
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    otp,
+  const newTodo = {
+    title,
+    description,
+    status: 'not completed',
+    createdAt: new Date(),
+    updatedAt: new Date()
   };
 
-  db.query('INSERT INTO users SET ?', userData, (err, result) => {
+  db.query('INSERT INTO todos SET ?', newTodo, (err, result) => {
     if (err) {
-      console.error('Error saving user data:', err);
-      res.status(500).json({ error: 'Error saving user data' });
+      console.error('Error creating Todo:', err);
+      res.status(500).json({ message: 'Internal server error' });
     } else {
-      // Send OTP to the user's email
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.GMAIL_USERNAME,
-          pass: process.env.GMAIL_PASSWORD,
-        },
-      });
-
-      const mailOptions = {
-        from: process.env.GMAIL_USERNAME,
-        to: email,
-        subject: 'OTP Verification',
-        text: `Your OTP for registration is: ${otp}`,
-      };
-
-      transporter.sendMail(mailOptions, (err) => {
-        if (err) {
-          console.error('Error sending email:', err);
-          res.status(500).json({ error: 'Error sending email' });
-        } else {
-          res.json({ message: 'User registered successfully', userId });
-        }
-      });
+      res.status(201).json({ message: 'Todo created successfully', id: result.insertId });
     }
   });
 });
 
-// Verify OTP and log in the user
-app.post('/login', (req, res) => {
-  const { email, otp } = req.body;
-
-  db.query(
-    'SELECT * FROM users WHERE email = ? AND otp = ?',
-    [email, otp],
-    (err, result) => {
-      if (err) {
-        console.error('Error fetching user data:', err);
-        res.status(500).json({ error: 'Error fetching user data' });
-      } else if (result.length === 0) {
-        res.status(401).json({ error: 'Invalid email or OTP' });
-      } else {
-        const user = result[0];
-        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        // TODO: You can generate and return a JWT token here for authentication
-        res.json({
-          message: 'Login successful',
-          user,
-          token,
-        });
-      }
+// Fetch all Todo items
+app.get('/todos', authorize, (req, res) => {
+  db.query('SELECT * FROM todos', (err, results) => {
+    if (err) {
+      console.error('Error fetching todos:', err);
+      res.status(500).json({ message: 'Internal server error' });
+    } else {
+      res.json(results);
     }
-  );
+  });
 });
 
+// Fetch Todo item by ID
+app.get('/todos/:id', authorize, (req, res) => {
+  const todoId = req.params.id;
 
+  db.query('SELECT * FROM todos WHERE id = ?', todoId, (err, results) => {
+    if (err) {
+      console.error('Error fetching todo:', err);
+      res.status(500).json({ message: 'Internal server error' });
+    } else if (results.length === 0) {
+      res.status(404).json({ message: 'Todo not found' });
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
 
+// Update Todo item by ID
+app.put('/todos/:id', authorize, (req, res) => {
+  const todoId = req.params.id;
+  const { title, description, status } = req.body;
 
-// Start the server
-app.listen(3001, () => {
-  console.log('Server started on port 3001');
+  const updatedTodo = {
+    title,
+    description,
+    status,
+    updatedAt: new Date()
+  };
+
+  db.query('UPDATE todos SET ? WHERE id = ?', [updatedTodo, todoId], (err, result) => {
+    if (err) {
+      console.error('Error updating todo:', err);
+      res.status(500).json({ message: 'Internal server error' });
+    } else if (result.affectedRows === 0) {
+      res.status(404).json({ message: 'Todo not found' });
+    } else {
+      res.json({ message: 'Todo updated successfully' });
+    }
+  });
+});
+
+// Delete Todo item by ID
+app.delete('/todos/:id', authorize, (req, res) => {
+  const todoId = req.params.id;
+
+  db.query('DELETE FROM todos WHERE id = ?', todoId, (err, result) => {
+    if (err) {
+      console.error('Error deleting todo:', err);
+      res.status(500).json({ message: 'Internal server error' });
+    } else if (result.affectedRows === 0) {
+      res.status(404).json({ message: 'Todo not found' });
+    } else {
+      res.json({ message: 'Todo deleted successfully' });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
